63d2ae077a4f2ca3cfcbcaa4374a6873b64d167d
Dennis <boadudennisx@gmail.com>
2026-05-16 18:22:41 +0000

    added url to livesite

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const convertBtn = document.getElementById("convert-btn")
 let lengthEl = document.getElementById("length-el")
 let volumeEl = document.getElementById("volume-el")
 let massEl = document.getElementById("mass-el")
-let inputVal = ""
 
 convertBtn.addEventListener("click", convertValue)
 
@@ -16,31 +15,22 @@ function convertValue() {
   lengthEl.innerHTML = "<h3>Length (Meter/Feet)</h3>"
   volumeEl.innerHTML = "<h3>Volume (Liters/Gallons)</h3>"
   massEl.innerHTML = "<h3>Mass (Kilograms/Pounds)</h3>"
-  inputVal = Number(inputEl.value) //convert input from string to number.
-
-  const metersToFeet = inputVal * 3.281
-  const feetToMeters = inputVal / 3.281
-  const litreToGallon = inputVal * 0.264
-  const gallonToLitre = inputVal / 0.264
-  const kgToPound = inputVal * 2.204
-  const poundToKg = inputVal / 2.204
+  const inputVal = Number(inputEl.value) //convert input from string to number.
 
   lengthEl.innerHTML += `<p>
-    ${inputVal} meters =  ${metersToFeet.toFixed(3)} feet |
-    ${inputVal} feet = ${feetToMeters.toFixed(3)} meters
+    ${inputVal} meters =  ${(inputVal * 3.281).toFixed(3)} feet |
+    ${inputVal} feet = ${(inputVal / 3.281).toFixed(3)} meters
   </p>
   `
   volumeEl.innerHTML += `<p>
-    ${inputVal} litres =  ${litreToGallon.toFixed(3)} gallon |
-    ${inputVal} gallon = ${gallonToLitre.toFixed(3)} litres
+    ${inputVal} litres =  ${(inputVal * 0.264).toFixed(3)} gallon |
+    ${inputVal} gallon = ${(inputVal / 0.264).toFixed(3)} litres
   </p>
   `
   massEl.innerHTML += `<p>
-    ${inputVal} kilos =  ${kgToPound.toFixed(3)} pounds |
-    ${inputVal} pounds = ${poundToKg.toFixed(3)} kilos
+    ${inputVal} kilos =  ${(inputVal * 2.204).toFixed(3)} pounds |
+    ${inputVal} pounds = ${(inputVal / 2.204).toFixed(3)} kilos
   </p>
   `
-
   inputEl.value = ""
-  inputVal = "" 
 }
